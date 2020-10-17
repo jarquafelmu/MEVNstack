c22695d3f74701d900e627e30772564914011315
jarquafelmu <thechaoselement@gmail.com>
2020-10-17 14:19:11 -0600

    navigation

---
+++ mevn-stack/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top custom-bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            alt="Vue logo"
+            src="../assets/logo.png"
+            style="max-height: 25px"
+          />
+          Task Manager
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto mb-2 mb-md-0">
+            <li class="nav-item active">
+              <a class="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tasks</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Logout</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sam</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: `Navbar`,
+};
+</script>
