5d284daa9b69568811600ffad746e2884a1aa104
jarquafelmu <thechaoselement@gmail.com>
2020-10-17 16:35:44 -0600

    tied router's link active class to bootstrap's active class

--- mevn-stack/src/router/index.js
+++ mevn-stack/src/router/index.js
@@ -44,12 +44,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  linkActiveClass: 'active'
 })
 
 export default router
