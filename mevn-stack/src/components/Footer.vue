acb52dc84b8e66b4b2ba3aa877f105bbecbdf8b6
jarquafelmu <thechaoselement@gmail.com>
2020-10-17 14:16:42 -0600

    footer element

---
+++ mevn-stack/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer id="custom-footer" class="px-2">
+    <span class="mr-4">&copy; 2020 Sam Terrazas</span>
+    <span class="mr-4">Private</span>
+    <span>Terms of Service</span>
+  </footer>
+</template>
+<script>
+export default {
+  name: `Footer`,
+};
+</script>
