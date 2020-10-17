51f27ffabd4bb062e851981480d2881e11aec96e
jarquafelmu <thechaoselement@gmail.com>
2020-10-17 16:59:19 -0600

    state management

--- mevn-stack/src/store/index.js
+++ mevn-stack/src/store/index.js
@@ -1,11 +1,29 @@
 import { createStore } from 'vuex'
+import * as auth from '../services/AuthServices'
 
 export default createStore({
   state: {
+    isLoggedIn: false,
+    apiURL: `http://localhost:3000`,
+    username: null,
+    userId: null
   },
   mutations: {
+    authenticate(state) {
+      state.isLoggedIn = auth.isLoggedIn();
+      if (state.isLoggedIn) {
+        state.username = auth.getUsername();
+        state.userId = auth.getUserId();
+      } else {
+        state.username = null;
+        state.userId = null;
+      }
+    }
   },
   actions: {
+    authenticate(context) {
+      context.commit(`authenticate`)
+    }
   },
   modules: {
   }
