c66afad7af223a885863b888c7fc2fe525aeb370
jarquafelmu <thechaoselement@gmail.com>
2020-10-17 17:06:14 -0600

    login form

--- mevn-stack/src/views/authentication/Login.vue
+++ mevn-stack/src/views/authentication/Login.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>Login Route</h1>
+  <div>
+    <h1>Login Route</h1>
+    <form class="custom-form">
+      <div class="mb-3">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          class="form-control"
+          id="username"
+          aria-describedby="username"
+          placeholder="Username"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 export default {
