45d5f32ce957fd501f90938786a8959d8db59abe
jarquafelmu <thechaoselement@gmail.com>
2020-10-18 15:37:43 -0600

    fun with dev and prod environments

--- mevn-stack/dev-server/index.js
+++ mevn-stack/dev-server/index.js
@@ -3,13 +3,25 @@ import express from 'express'
 const app = express()
 const port = 3000
 import { registerRoutes } from './routes'
+import { setEnvironment } from './config/env'
 
+if (process.env.NODE_ENV)
+  process.env.NODE_ENV = process.env.NODE_ENV.trim()
+
+setEnvironment(app);
 registerRoutes(app);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  if (process.env.NODE_ENV !== `production`)
+    return res.send(
+      `Running server in development mode`
+    )
+  else
+    return res.sendFile(`index.html`, { root: `${__dirname}/../dist/` })
 })
 
 app.listen(port, () => {
-  console.log(`Example apps listening at http://localhost:${port}`)
+  console.log(
+    `MEVN app listening at http://localhost:${port}${(process.env.NODE_ENV) ? `in ${process.env.NODE_ENV}` : ''}}`
+  )
 })
--- mevn-stack/prod-server/index.js
+++ mevn-stack/prod-server/index.js
@@ -6,18 +6,23 @@ var _express2 = _interopRequireDefault(_express);
 
 var _routes = require('./routes');
 
+var _env = require('./config/env');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 var port = 3000;
 
 
+if (process.env.NODE_ENV) process.env.NODE_ENV = process.env.NODE_ENV.trim();
+
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  if (process.env.NODE_ENV !== 'production') return res.send('Running server in development mode');else return res.sendFile('index.html', { root: __dirname + '/../dist/' });
 });
 
 app.listen(port, function () {
-  console.log('Example apps listening at http://localhost:' + port);
+  console.log('MEVN app listening at http://localhost:' + port + (process.env.NODE_ENV ? 'in ' + process.env.NODE_ENV : '') + '}');
 });
