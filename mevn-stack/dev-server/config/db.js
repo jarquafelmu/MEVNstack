aa3c32ec0160a0f2293b014d424f60e8fc1ce226
jarquafelmu <thechaoselement@gmail.com>
2020-10-24 12:13:57 -0600

    added option to fix deprecation warning

--- mevn-stack/dev-server/config/db.js
+++ mevn-stack/dev-server/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export function connectToDB() {
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true },
+  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true },
     error => {
       if (error) {
         console.log(`Unable to connect to database`)
