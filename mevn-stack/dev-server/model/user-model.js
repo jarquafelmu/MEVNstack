438ff9ed6cb7c2979aefe8d42092d4633ac1b281
jarquafelmu <thechaoselement@gmail.com>
2020-10-23 19:42:58 -0600

    flesh out user schema and actions

--- mevn-stack/dev-server/model/user-model.js
+++ mevn-stack/dev-server/model/user-model.js
@@ -1,4 +1,6 @@
 import mongoose, { Mongoose } from 'mongoose'
+import { StringUtil } from '../utilities/string-util'
+
 const userSchema = new Mongoose.Schema({
   username: String,
   first: String,
@@ -6,5 +8,16 @@ const userSchema = new Mongoose.Schema({
   password: String,
 });
 userSchema.set(`timestamps`, true)
+userSchema.virtual('fullName').get(function () {
+  const first = StringUtil.capitalize(this.first.toLowerCase());
+  const last = StringUtil.capitalize(this.last.toLowerCase());
+  return `${first} ${last}`
+})
+userSchema.pre('save', function (next) {
+  this.username = this.username.toLowerCase();
+  this.first = this.first.toLowerCase();
+  this.last = this.last.toLowerCase();
+  next();
+})
 
 export default mongoose.model('user', userSchema);
