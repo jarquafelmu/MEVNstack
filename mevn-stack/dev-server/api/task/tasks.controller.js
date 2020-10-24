4496f72a04fcadcda3a1d3a59e2895fe52c020d7
jarquafelmu <thechaoselement@gmail.com>
2020-10-23 20:37:32 -0600

    Finished U (update) of CRUD

--- mevn-stack/dev-server/api/task/tasks.controller.js
+++ mevn-stack/dev-server/api/task/tasks.controller.js
@@ -14,11 +14,13 @@ export function index(req, res) {
 }
 export function create(req, res) {
   // CREATE TASK
-  const id = 10;
+  const id = 10;  // temporary fake id for testing
   User.findOne({ _id: id }, (error, user) => {
-    if (error && !user) {
-      return res.status(500).json()
-    }
+    if (error)
+      return res.status(500).json();
+    if (!user)
+      return res.status(404).json();
+
     const task = new Task(req.body.task);
     task.author = user._id;
     task.dueDate = moment(task.dueDate);
@@ -34,7 +36,25 @@ export function create(req, res) {
 
 export function update(req, res) {
   // UPDATE TASK
-  return res.status(204).json();
+  const id = 10; // temporary fake id for testing
+  User.findOne({ _id: id }, (error, user) => {
+    if (error)
+      return res.status(500).json();
+    if (!user)
+      return res.status(404).json();
+
+    const task = req.body.task;
+    task.author = user._id;
+    task.dueDate = moment(task.dueDate);
+    Task.findByIdAndUpdate({ _id: task._id }, (error, task) => {
+      if (error)
+        return res.status(500).json();
+      if (!task)
+        return res.status(404).json();
+
+      return res.status(204).json();
+    })
+  })
 }
 
 export function remove(req, res) {
